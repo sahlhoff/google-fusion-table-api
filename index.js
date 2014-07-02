990be203ea8da7c3f5d17dc20266c33ce252c5b9
Chad Sahlhoff <csahlhoff@gmail.com>
2014-07-01 22:28:08 -0700

    added clientlogin auth

--- index.js
+++ index.js
@@ -2,43 +2,149 @@
 
 var request = require('request');
 var util = require('util');
+var GoogleClientLogin = require('googleclientlogin').GoogleClientLogin;
+
+function FusionError (error){
+  Error.captureStackTrace(this, FusionError)
+  this.error = error;
+}
+
+util.inherits(FusionError, Error);
+
+FusionError.prototype.toString = function () {
+  return 'Fusion Error: ' + this.error;
+}
 
 function Fusion (options) {
-  this.baseUrl = 'https://www.googleapis.com/fusiontables/v1/query/';
+  if(!options || !options.apiKey)
+    throw new FusionError('Must have API Key to use Fusion Tables');
+  this.apiKey = options.apiKey;
+  this.baseUrl = 'https://www.googleapis.com/fusiontables/v1/';
+  this.googleAuth = '';
 
   var self = this;
 
   function get (url, callback){
-    request.get(url, function(err, res, data){
+    console.log('self.googleAuth = ', self.googleAuth);
+    var reqObject = {
+      url: url,
+      headers: {
+        'Authorization': 'GoogleLogin auth='+self.googleAuth
+      }
+    }
+    request.get(reqObject, function (err, res, data){
       if(err){
-        callback(err);
+        return callback(err);
       } else {
         try {
           data = JSON.parse(data);
-          if(data.success === false){
-            callback(data.errors || data.error);
+          if(data.error || data.errors){
+            return callback(data.errors || data.error);
           } else {
-            callback(null, data);
+            return callback(null, data);
           }
         } catch(err){
-          callback(err);
+          return callback(err);
         }
       }
-    })
+    });
   }
 
   function post (url, param, callback){
+    var body = JSON.stringify(param);
+    var reqObject = {
+      url: url,
+      body: body
+    }
 
+    request.post(reqObject, function (err, res, data){
+      if(err){
+        return callback(err);
+      } else {
+        try {
+          data = JSON.parse(data);
+          if(data.error || data.errors){
+            return callback(data.errors || data.error);
+          } else {
+            return callback(null, data);
+          }
+        } catch(err){
+          return callback(err);
+        }
+      }
+    });
+  }
+
+  //Client Login
+  this.clientLogin = function (username, password){
+    var googleAuth = new GoogleClientLogin({
+      email: username,
+      password: password,
+      service: 'fusiontables',
+      accountType: GoogleClientLogin.accountTypes.google
+    });
+
+    googleAuth.on(GoogleClientLogin.events.login, function(){
+      self.googleAuth = googleAuth.getAuthId();
+      console.log('auth ', self.googleAuth);
+    });
+
+    googleAuth.login();
   }
 
 
-  this.list = function(callback){
-    var url = self.baseUrl + 'tables';
+  this.tables = {};
 
-    console.log('GET', url);
-
+  //GET tables
+  this.tables.list = function (callback){
+    var url = self.baseUrl+'tables?key='+self.apiKey;
     get(url, callback);
   }
+
+  this.tables.table = function (tableId, callback){
+    var url = self.baseUrl+'tables/'+tableId+'?key='+self.apiKey;
+    get(url, callback);
+  }
+
+  this.tables.columns = function (tableId, callback){
+    var url = self.baseUrl+'tables/'+tableId+'/columns?key='+self.apiKey;
+    get(url, callback);
+  }
+
+  this.tables.column = function (tableId, columnId, callback){
+    var url = self.baseUrl+'tables/'+tableId+'/columns/'+columnId+'?key='+self.apiKey;
+    get(url, callback);
+  }
+
+  this.tables.templates = function (tableId, callback){
+    var url = self.baseUrl+'tables/'+tableId+'/templates?key='+self.apiKey;
+    get(url, callback);
+  }
+
+  this.tables.template = function (tableId, templateId, callback){
+    var url = self.baseUrl+'tables/'+tableId+'/templates/'+templateId+'?key='+self.apiKey;
+    get(url, callback);
+  }
+
+  this.tables.styles = function (tableId, callback){
+    var url = self.baseUrl+'tables/'+tableId+'/styles?key='+self.apiKey;
+    get(url, callback);
+  }
+
+  this.tables.style = function (tableId, styleId, callback){
+    var url = self.baseUrl+'tables/'+tableId+'/styles/'+styleId+'?key='+self.apiKey;
+    get(url, callback);
+  }
+
+  //POST tables
+  this.tables.create = function (callback){
+    var url = self.baseUrl+'tables?key='+self.apiKey;
+    console.log(url);
+    post(url, '', callback);
+  }
+
+  this.query = {};
+
 
 
 }
--- test/test.js
+++ test/test.js
@@ -1,9 +1,90 @@
 var Fusion = require('../index');
-var should = require('should');
+             require('should');
 
-var fusion = new Fusion();
+var fusion = new Fusion({apiKey: 'AIzaSyAz9_gvrnxWRSgaV0rel_P4FJfEknO9Lu8'});
+var tableId = '12qN6I21grayn8UyEgijv3um4vbbAiyqIk9mnxgmK';
 
-fusion.list(function (err, data){
-  if(err){console.log(err)}
-  console.log(data);
+fusion.clientLogin('csahlhoff@gmail.com', 'Bremen321');
+
+describe('fusion.tables.table', function(){
+  it('should return ', function (done){
+    fusion.tables.table(tableId, function (err, data){
+      data.should.have.property('kind');
+      done();
+    });
+  });
+});
+
+describe('fusion.tables.columns', function(){
+  it('should return ', function (done){
+    fusion.tables.columns(tableId, function (err, data){
+      data.should.have.property('kind');
+      done();
+    });
+  });
+});
+
+describe('fusion.tables.column', function(){
+  it('should return ', function (done){
+    fusion.tables.column(tableId, '0', function (err, data){
+      data.should.have.property('kind');
+      done();
+    });
+  });
+});
+
+describe('fusion.tables.templates', function(){
+  it('should return ', function(done){
+    fusion.tables.templates(tableId, function (err, data){
+      data.should.have.property('kind');
+      done();
+    });
+  });
+});
+
+describe('fusion.tables.template ', function(){
+  it('should return ', function (done){
+    fusion.tables.template(tableId, '1', function (err, data){
+      data.should.have.property('kind');
+      done();
+    });
+  });
+});
+
+describe('fusion.tables.styles ', function(){
+  it('should return ', function (done){
+    fusion.tables.styles(tableId, function (err, data){
+      data.should.have.property('kind');
+      done();
+    });
+  });
+});
+
+describe('fusion.table.style ', function(){
+  it('should return ', function (done){
+    fusion.tables.style(tableId, '1', function (err, data){
+      data.should.have.property('kind');
+      done();
+    });
+  });
+});
+
+describe('fusion.tables.list', function(){
+  it('should return ', function (done){
+    fusion.tables.list(function (err, data){
+      if(err){console.log('err ', err)}
+      console.log('data ', data);
+      done();
+    });
+  });
+});
+
+describe('fusion.table.create', function(){
+  it('should create a table and return ', function(done){
+    fusion.tables.create(function (err, data){
+      if(err){console.log(err)}
+      console.log(data);
+      done();
+    })
+  })
 })
